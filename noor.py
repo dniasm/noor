175f7fe20d988997115992a488c230c6a67535c0
Asmin Dani <asmindani9@gmail.com>
2026-04-28 17:19:28 +0545

    initial commit - noor conversational AI

---
+++ noor.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def chat(message):
+    url="http://localhost:11434/api/generate"
+
+    data = {
+        "model" : "mistral",
+        "prompt" : message,
+        "stream" : False
+    }
+
+    response = requests.post(url, json=data)
+    return json.loads(response.text)["response"]
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower == "quit":
+        break
+    response = chat(user_input)
+    print(f"Noor: {response}\n")
